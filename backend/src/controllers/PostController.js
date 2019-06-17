c90f15f187174cc3263e7099936d23a68b4c0599
wagnercandido <wagnercssilva@gmail.com>
2019-06-17 08:56:40 -0300

    Resolvendo erro imagem

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -10,30 +10,30 @@ module.exports = {
     },
 
     async store(req, res) {
-        const { author, place, description, hashtags} = req.body;
+        const { author, place, description, hashtags } = req.body;
         const { filename: image } = req.file;
 
-        const [name] = image.split('.');
-        const fileName = `${name}.jpg`;
+        // const [name] = image.split('.');
+        // const fileName = `${name}.jpg`;
 
         await sharp(req.file.path)
-        .resize(500)
-        .jpeg({quality: 70})
-        .toFile(
-            path.resolve(req.file.destination, 'resized', fileName)
-        )
-
-        // fs.unlinkSync(req.file.path);
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            )
 
         const post = await Post.create({
             author,
             place,
             description,
             hashtags,
-            fileName
+            image
         });
 
         req.io.emit('post', post);
+
+        fs.unlinkSync(req.file.path);
 
         return res.json({ post })
     }
